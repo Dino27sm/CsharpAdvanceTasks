3b8b9194c5c06791424d9134e8197bc8c95f570a
Mironov <mironov5712s@gmail.com>
2020-08-29 20:27:44 +0300

    Create Program.cs

---
+++ Advance17Sept2020StacksAndQueuesExercise/T07.TruckTourM1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace T07.TruckTourM1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<long> queueDiff = new Queue<long>();
+            int numStations = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numStations; i++)
+            {
+                long[] readLine = Console.ReadLine().Split().Select(long.Parse).ToArray();
+                queueDiff.Enqueue(readLine[0] - readLine[1]);
+            }
+            int startIndex = 0;
+            for (int i = 0; i < numStations; i++)
+            {
+                bool flag = true;
+                long tempOut = queueDiff.Peek();
+                if (tempOut >= 0)
+                {
+                    long sumDiff = 0;
+                    for (int k = 0; k < numStations; k++)
+                    {
+                        long tempTwo = queueDiff.Dequeue();
+                        sumDiff += tempTwo;
+                        queueDiff.Enqueue(tempTwo);
+                        if(sumDiff < 0)
+                            flag = false;
+                    }
+                    if (flag)
+                    {
+                        startIndex = i;
+                        break;
+                    }
+                    queueDiff.Enqueue(queueDiff.Dequeue());
+                }
+                else
+                    queueDiff.Enqueue(queueDiff.Dequeue());
+            }
+            Console.WriteLine(startIndex);
+        }
+    }
+}
