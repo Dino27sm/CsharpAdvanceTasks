fa99acf2753c4762061177474459c7b8292e76c1
Mironov <mironov5712s@gmail.com>
2020-08-30 12:43:39 +0300

    Create Program.cs

---
+++ Advance17Sept2020StacksAndQueuesExercise/08.BalancedParenthesis/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08.BalancedParenthesis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string readParentheses = Console.ReadLine();
+            Stack<char> stackParentheses = new Stack<char>();
+            for (int i = 0; i < readParentheses.Length; i++)
+            {
+                char currentChar = readParentheses[i];
+                if (currentChar == '{' || currentChar == '[' || currentChar == '(')
+                    stackParentheses.Push(readParentheses[i]);
+                else if (currentChar == '}' || currentChar == ']' || currentChar == ')')
+                {
+                    if (currentChar == '}') currentChar = '{';
+                    else if (currentChar == ']') currentChar = '[';
+                    else if (currentChar == ')') currentChar = '(';
+                    if (stackParentheses.Any() && stackParentheses.Peek() == currentChar)
+                        stackParentheses.Pop();
+                    else
+                    {
+                        Console.WriteLine("NO");
+                        return;
+                    }
+                }
+            }
+            if (stackParentheses.Any())
+                Console.WriteLine("NO");
+            else
+                Console.WriteLine("YES");
+        }
+    }
+}
