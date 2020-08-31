f7258e35c232f084f69ef31b7a396b54df1ff5d8
Mironov <mironov5712s@gmail.com>
2020-08-31 13:52:31 +0300

    Create a task solution

---
+++ Advance17Sept2020StacksAndQueuesExercise/T10.Crossroads/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace T10.Crossroads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> carQueue = new Queue<string>();
+            int countCars = 0;
+            int greenTime = int.Parse(Console.ReadLine());
+            int yellowTime = int.Parse(Console.ReadLine());
+            string carName = "";
+            while ((carName = Console.ReadLine()) != "END")
+            {
+                if (carName == "green")
+                {
+                    int greenLeft = greenTime;
+                    while (carQueue.Any() && greenLeft > 0)
+                    {
+                        string carToPass = carQueue.Peek();
+                        if (carToPass.Length <= greenLeft)
+                        {
+                            carQueue.Dequeue();
+                            greenLeft -= carToPass.Length;
+                            countCars++;
+                        }
+                        else
+                        {
+                            int carPart = carToPass.Length - greenLeft;
+                            if (carPart <= yellowTime)
+                            {
+                                carQueue.Dequeue();
+                                countCars++;
+                                break;
+                            }
+                            else
+                            {
+                                int indexHit = greenLeft + yellowTime;
+                                Console.WriteLine("A crash happened!");
+                                Console.WriteLine($"{carToPass} was hit at {carToPass[indexHit]}.");
+                                return;
+                            }
+                        }
+                    }
+                }
+                else carQueue.Enqueue(carName);
+            }
+            Console.WriteLine("Everyone is safe.");
+            Console.WriteLine($"{countCars} total cars passed the crossroads.");
+        }
+    }
+}
