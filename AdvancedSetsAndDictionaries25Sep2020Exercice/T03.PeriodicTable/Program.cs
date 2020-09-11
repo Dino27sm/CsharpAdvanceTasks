8a6328c2fcb60cf67c6e81aa9b01e8be8b065ed1
Mironov <mironov5712s@gmail.com>
2020-09-11 21:29:01 +0300

    Create Program.cs

---
+++ AdvancedSetsAndDictionaries25Sep2020Exercice/T03.PeriodicTable/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace T03.PeriodicTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedSet<string> chemicElements = new SortedSet<string>();
+            int numLines = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numLines; i++)
+            {
+                string[] readLine = Console.ReadLine().Split();
+                for (int k = 0; k < readLine.Length; k++)
+                {
+                    chemicElements.Add(readLine[k]);
+                }
+            }
+            Console.WriteLine(string.Join(" ", chemicElements));
+        }
+    }
+}
