5951595d51ef3b77f73da11c31132f2a685a5431
Mironov <mironov5712s@gmail.com>
2020-09-12 07:08:44 +0300

    Create Program.cs

---
+++ AdvancedSetsAndDictionaries25Sep2020Exercice/T06.WardrobeM1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace T06.WardrobeM1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Dictionary<string, int>> dressColor = new Dictionary<string, Dictionary<string, int>>();
+            int numLines = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numLines; i++)
+            {
+                string[] infoParts = Console.ReadLine().Split(" -> ");
+                string getColor = infoParts[0];
+                string[] currentDresses = infoParts[1].Split(",");
+                if (!dressColor.ContainsKey(getColor))
+                {
+                    dressColor.Add(getColor, new Dictionary<string, int>());
+                }
+                for (int k = 0; k < currentDresses.Length; k++)
+                {
+                    if (!dressColor[getColor].ContainsKey(currentDresses[k]))
+                    {
+                        dressColor[getColor].Add(currentDresses[k], 0);
+                    }
+                    dressColor[getColor][currentDresses[k]]++;
+                }
+            }
+            string[] findClothes = Console.ReadLine().Split();
+            string colorFind = findClothes[0];
+            string dressFind = findClothes[1];
+            foreach (var kvp in dressColor)
+            {
+                Console.WriteLine($"{kvp.Key} clothes:");
+                foreach (var item in kvp.Value)
+                {
+                    if (kvp.Key == colorFind && item.Key == dressFind)
+                    {
+                        Console.WriteLine($"* {item.Key} - {item.Value} (found!)");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"* {item.Key} - {item.Value}");
+                    }
+                }
+            }
+        }
+    }
+}
