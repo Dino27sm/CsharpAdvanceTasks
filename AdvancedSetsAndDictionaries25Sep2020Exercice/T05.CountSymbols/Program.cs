6052542bfe81af2304406c58f3cbcf33453c5fb4
Mironov <mironov5712s@gmail.com>
2020-09-12 07:07:22 +0300

    Create Program.cs

---
+++ AdvancedSetsAndDictionaries25Sep2020Exercice/T05.CountSymbols/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace T05.CountSymbols
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<char, int> charData = new SortedDictionary<char, int>();
+            char[] readText = Console.ReadLine().ToCharArray();
+            for (int i = 0; i < readText.Length; i++)
+            {
+                if (!charData.ContainsKey(readText[i]))
+                {
+                    charData.Add(readText[i], 0);
+                }
+                charData[readText[i]]++;
+            }
+            foreach (var kvp in charData)
+            {
+                Console.WriteLine($"{kvp.Key}: {kvp.Value} time/s");
+            }
+        }
+    }
+}
