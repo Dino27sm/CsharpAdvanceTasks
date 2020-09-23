de82047560f9d997ddc352540a6ff3e22919dddc
Mironov <mironov5712s@gmail.com>
2020-09-23 19:28:16 +0300

    Create Program.cs

---
+++ AdvancedFunctionalProgramming02Oct2020Exercise/T01.ActionPrint/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace T01.ActionPrint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] readWords = Console.ReadLine().Split(" ");
+            Action<string> printText = textToPrint => Console.WriteLine(textToPrint);
+            foreach (string word in readWords)
+                printText(word);
+        }
+    }
+}
