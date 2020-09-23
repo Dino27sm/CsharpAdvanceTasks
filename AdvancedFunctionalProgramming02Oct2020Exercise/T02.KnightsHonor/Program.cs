643f3d4c198b9250c1a983b9655aa007ac5cb753
Mironov <mironov5712s@gmail.com>
2020-09-23 19:31:06 +0300

    Create Program.cs

---
+++ AdvancedFunctionalProgramming02Oct2020Exercise/T02.KnightsHonor/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace T02.KnightsHonor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] readNames = Console.ReadLine().Split(" ");
+            Func<string, string> addTitle = getName => "Sir " + getName;
+            foreach (string names in readNames)
+                Console.WriteLine(addTitle(names));
+        }
+    }
+}
