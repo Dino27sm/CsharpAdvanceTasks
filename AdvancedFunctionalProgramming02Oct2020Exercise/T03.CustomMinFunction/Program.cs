52adbfeaa228390e76e70ee47b9f0866fe6b1357
Mironov <mironov5712s@gmail.com>
2020-09-24 20:36:56 +0300

    Create Program.cs

---
+++ AdvancedFunctionalProgramming02Oct2020Exercise/T03.CustomMinFunction/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace T03.CustomMinFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] readNum = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Func<int[], int> getMin = number => number.Min();
+            int getResult = getMin(readNum);
+            Console.WriteLine(getResult);
+        }
+    }
+}
