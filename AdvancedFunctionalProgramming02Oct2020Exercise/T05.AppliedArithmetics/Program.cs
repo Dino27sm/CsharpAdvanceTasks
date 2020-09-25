8264c862ad89ef646a013119f7999d941ee63086
Mironov <mironov5712s@gmail.com>
2020-09-25 12:02:41 +0300

    Create Program.cs

---
+++ AdvancedFunctionalProgramming02Oct2020Exercise/T05.AppliedArithmetics/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace T05.AppliedArithmetics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<List<double>> printAction = getList => Console.WriteLine(string.Join(" ", getList));
+            Func<List<double>, List<double>> addOne = inpList => inpList.Select(x => x + 1).ToList();
+            Func<List<double>, List<double>> multiplyByTwo = inpList => inpList.Select(x => x * 2).ToList();
+            Func<List<double>, List<double>> subtractOne = inpList => inpList.Select(x => x - 1).ToList();
+
+            List<double> allNum = Console.ReadLine().Split().Select(double.Parse).ToList();
+            string command = "";
+            while ((command = Console.ReadLine()) != "end")
+            {
+                if (command == "add")
+                {
+                    allNum = addOne(allNum).ToList();
+                }
+                else if (command == "multiply")
+                {
+                    allNum = multiplyByTwo(allNum).ToList();
+                }
+                else if (command == "subtract")
+                {
+                    allNum = subtractOne(allNum).ToList();
+                }
+                else if (command == "print")
+                {
+                    printAction(allNum);
+                }
+            }
+        }
+    }
+}
