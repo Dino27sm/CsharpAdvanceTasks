89db35c07e7d1e8bc9c6f74d5d0c2185046e2a99
Mironov <mironov5712s@gmail.com>
2020-09-25 19:25:56 +0300

    Create Program.cs

---
+++ AdvancedFunctionalProgramming02Oct2020Exercise/T06.ReverseAndExclude/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace T06.ReverseAndExclude
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<List<int>, int, List<int>> whichNotDevided = (x, y) => x.Where(z => z % y != 0).ToList();
+            List<int> readInput = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int devidor = int.Parse(Console.ReadLine());
+            readInput.Reverse();
+            readInput = whichNotDevided(readInput, devidor).ToList();
+            Console.WriteLine(string.Join(" ", readInput));
+        }
+    }
+}
