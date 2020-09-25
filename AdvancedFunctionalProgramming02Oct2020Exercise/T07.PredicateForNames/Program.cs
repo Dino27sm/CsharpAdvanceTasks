a819e15f64b4e516bda4e70c305167442080dea8
Mironov <mironov5712s@gmail.com>
2020-09-25 19:26:42 +0300

    Create Program.cs

---
+++ AdvancedFunctionalProgramming02Oct2020Exercise/T07.PredicateForNames/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace T07.PredicateForNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int readNum = int.Parse(Console.ReadLine());
+            List<string> getNames = Console.ReadLine().Split().ToList();
+            Func<List<string>, int, List<string>> checkNames = (x, y) => x.Where(z => z.Length <= y).ToList();
+            Console.WriteLine(string.Join(Environment.NewLine, checkNames(getNames, readNum)));
+        }
+    }
+}
