62b9037acc8331f876ff52a28cfad24fc84e8a0d
Mironov <mironov5712s@gmail.com>
2020-09-25 11:21:44 +0300

    Create Program.cs

---
+++ AdvancedFunctionalProgramming02Oct2020Exercise/T04.FindEvensOddsM1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace T04.FindEvensOddsM1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Predicate<int> isOdd = getNum => Math.Abs(getNum) % 2 == 1;
+            int[] numRange = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            string whatType = Console.ReadLine();
+            int firsttNum = numRange[0];
+            int lastNum = numRange[1];
+            int[] allNumbers = new int[lastNum - firsttNum + 1];
+            for (int i = 0; i < allNumbers.Length; i++)
+            {
+                allNumbers[i] = firsttNum + i;
+            }
+            allNumbers = whatType == "odd" ? allNumbers.Where(x => isOdd(x)).ToArray() : allNumbers = allNumbers.Where(x => !isOdd(x)).ToArray();
+            Console.WriteLine(string.Join(" ", allNumbers));
+        }
+    }
+}
