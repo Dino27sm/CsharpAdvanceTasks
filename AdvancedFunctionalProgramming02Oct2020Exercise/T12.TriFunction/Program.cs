7bba8d5657b961dfa60418c2fb031177282c5f43
Mironov <mironov5712s@gmail.com>
2020-09-27 11:09:25 +0300

    Create Program.cs

---
+++ AdvancedFunctionalProgramming02Oct2020Exercise/T12.TriFunction/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace T12.TriFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int readSumChar = int.Parse(Console.ReadLine());
+            List<string> listNames = Console.ReadLine().Split().ToList();
+            Func<string, int> returnCharSum = inpString => inpString.Select(x => (int)x).Sum();
+            for (int i = 0; i < listNames.Count; i++)
+            {
+                if(returnCharSum(listNames[i]) >= readSumChar)
+                {
+                    Console.WriteLine(listNames[i]);
+                    break;
+                }
+            }
+        }
+    }
+}
