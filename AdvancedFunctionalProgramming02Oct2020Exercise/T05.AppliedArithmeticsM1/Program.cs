aeb49b3372d87773e79f4e75b23c527d230d0a31
Mironov <mironov5712s@gmail.com>
2020-10-03 08:37:53 +0300

    Create Program.cs

---
+++ AdvancedFunctionalProgramming02Oct2020Exercise/T05.AppliedArithmeticsM1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace T05.AppliedArithmeticsM1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inpArray = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            string commandRead = "";
+            while((commandRead = Console.ReadLine()) != "end")
+            {
+                if (commandRead == "print")
+                {
+                    Console.WriteLine(string.Join(" ", inpArray));
+                    continue;
+                }
+                inpArray = ManipulateArray(inpArray, commandRead);
+            }
+        }
+        static int[] ManipulateArray(int[] inpArray, string command)
+        {
+            int[] outArray = inpArray.ToArray();
+            if (command == "add") outArray = outArray.Select(x => x + 1).ToArray();
+            else if (command == "subtract") outArray = outArray.Select(x => x - 1).ToArray();
+            else if (command == "multiply") outArray = outArray.Select(x => x * 2).ToArray();
+            return outArray;
+        }
+    }
+}
