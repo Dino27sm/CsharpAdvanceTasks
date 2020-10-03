360aeec17b5a0181aad878bac03539f6464b42ad
Mironov <mironov5712s@gmail.com>
2020-10-03 08:38:41 +0300

    Create Program.cs

---
+++ AdvancedFunctionalProgramming02Oct2020Exercise/T05.AppliedArithmeticsM2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace T05.AppliedArithmeticsM2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inpArr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Action<int[]> printArray = arrayToPrint => Console.WriteLine(string.Join(" ", arrayToPrint));
+            string getCommand = "";
+            Func<int, int> manipulateNum = num => ManipulateNumber(num, getCommand);
+            while((getCommand = Console.ReadLine()) != "end")
+            {
+                if (getCommand == "print") printArray(inpArr);
+                else inpArr = inpArr.Select(x => manipulateNum(x)).ToArray();
+            }
+        }
+        static int ManipulateNumber(int inpNum, string command)
+        {
+            if (command == "add") inpNum++;
+            if (command == "subtract") inpNum--;
+            if (command == "multiply") inpNum *= 2;
+            return inpNum;
+        } 
+    }
+}
